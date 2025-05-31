f2761791ca560ac866639939646e9ed415f507be
madhankumarchavan <madhankumarchavan26@gmail.com>
2025-05-31 12:55:14 +0530

    topics test

--- frontend.sh
+++ frontend.sh
@@ -6,7 +6,7 @@ systemctl start nginx
 
 rm -rf /usr/share/nginx/html/*
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
 
 cd /usr/share/nginx/html
