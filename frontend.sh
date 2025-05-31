73ed16ccafe508e249ac335c8f79cfc48d3effc7
madhankumarchavan <madhankumarchavan26@gmail.com>
2025-05-31 19:51:13 +0530

    topics test

--- frontend.sh
+++ frontend.sh
@@ -5,21 +5,64 @@ LOG=/tmp/roboshop.log
 
 
 dnf install nginx -y &>>${LOG}
+if [ $? -eq 0 ]; then
+  echo sucuess
+else
+    echo failure
+fi
 
 systemctl enable nginx &>>${LOG}
+if [ $? -eq 0 ]; then
+  echo sucuess
+else
+    echo failure
+fi
 systemctl start nginx &>>${LOG}
-
+if [ $? -eq 0 ]; then
+  echo sucuess
+else
+    echo failure
+fi
 
 rm -rf /usr/share/nginx/html/* &>>${LOG}
-
+if [ $? -eq 0 ]; then
+  echo sucuess
+else
+    echo failure
+fi
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
-
+if [ $? -eq 0 ]; then
+  echo sucuess
+else
+    echo failure
+fi
 
 cd /usr/share/nginx/html &>>${LOG}
+if [ $? -eq 0 ]; then
+  echo sucuess
+else
+    echo failure
+fi
+
 unzip /tmp/frontend.zip &>>${LOG}
+if [ $? -eq 0 ]; then
+  echo sucuess
+else
+    echo failure
+fi
 
 cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
+if [ $? -eq 0 ]; then
+  echo sucuess
+else
+    echo failure
+fi
 
 systemctl enable nginx &>>${LOG}
 
 systemctl restart nginx &>>${LOG}
+if [ $? -eq 0 ]; then
+  echo sucuess
+else
+    echo failure
+fi
