a0c2b0c5bd0f23bb599d217d760751616287828e
madhankumarchavan <madhankumarchavan26@gmail.com>
2025-05-31 13:12:21 +0530

    topics test

--- frontend.sh
+++ frontend.sh
@@ -1,3 +1,8 @@
+
+
+script_location:$(pwd)
+
+
 dnf install nginx -y
 
 systemctl enable nginx
@@ -12,10 +17,7 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.z
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-cd /
-cp /files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
-
-#cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
+cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 
 
 systemctl enable nginx
